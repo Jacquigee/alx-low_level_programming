fefb2b87194258da7679fd9b47be903fee298263
Jacquigee <gitaujaquiline@gmail.com>
2022-03-21 12:12:40 -0700

    Adding file 100

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - print prime factors
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	long int i, p, fp = 612852475143;
+
+	i = 2;
+	while (i <= 612852475143 && fp > 1)
+	{
+		p = i;
+		if ((fp % p) == 0)
+			fp = fp / p;
+		else
+			i++;
+
+	}
+	printf("%ld\n", p);
+
+	return (0);
+}
