01420e0b4c27836e2f5e7dd8dc4e4594a1598c4d
Jacquigee <gitaujaquiline@gmail.com>
2022-05-15 11:46:15 -0700

    Adding file 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main(void) __attribute__ ((constructor));
+
+/**
+ * main - check the code
+ * Return: 0 Always
+ */
+void main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
